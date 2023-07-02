c7bbc72f5c2a6d2920cd430d92e30fd4d7ef7f77
hongyan <hyhome1003@gmail.com>
2023-07-02 16:47:39 +0800

    init finish

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">机房demo</router-link> |
+    <router-link to="/threejsdemo">threejs demo</router-link>
   </nav>
-  <router-view/>
+  <router-view />
 </template>
 
 <style>
